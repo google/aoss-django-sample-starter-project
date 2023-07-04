5297b57b74300014410da0e960849000770560ac
Rubalien30 <134284199+Rubalien30@users.noreply.github.com>
2023-07-04 16:54:19 +0530

    Update helloworld.py

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 from loguru import logger
 
 def hello(request):
-    logger.info("Application Started")
+    logger.info("Welcome to AOSS-Django-Sample-Starter-Project!")
 
     return HttpResponse("Hello, World!")
