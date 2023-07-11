93a7bb1de40fb6d19ee94a6e5c8b294c5b83d364
Rubal Tripathi <rubalien@google.com>
2023-07-11 08:53:31 +0000

    added update report generator

--- generate_report.sh
+++ generate_report.sh
@@ -14,52 +14,55 @@
 
 credentials_file="$1"
 export GOOGLE_APPLICATION_CREDENTIALS="$credentials_file"
-# Run pip list command and store the output in a file
-pip list --format=columns requests | awk 'NR>2 {print $1":"$2}' > report.txt
 
-# Variables to store packages from Assured OSS and Open Source
-aoss_packages=()
-os_packages=()
+pip list --format=columns requests | awk 'NR>2 {print $1":"$2}' > tempfile_output_pip
 
-# Read each package from the file and generate the URL
-while IFS= read -r package; do
-    # Remove any spaces in the package
-    package=${package// /}
+# Convert pip dependencies into URL format and store in a temporary file
+sed -i 's/\(.*\)"\(.*\)"/\2/g' tempfile_output_pip
+sed -i 's/\(.*\)/"name": "projects\/cloud-aoss\/locations\/us\/repositories\/cloud-aoss-python\/pythonPackages\/\1",/g' tempfile_output_pip
 
-    # Construct the URL for the package
-    url="https://artifactregistry.googleapis.com/v1/projects/cloud-aoss/locations/us/repositories/cloud-aoss-python/pythonPackages/$package"
+# Store the output of the cURL command in a file
+curl_output_first=$(curl -X GET -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) \
+  "https://artifactregistry.googleapis.com/v1/projects/cloud-aoss/locations/us/repositories/cloud-aoss-python/pythonPackages?pageSize=2000" \
+  | grep name | sort -f)
 
-    # Execute the cURL command and capture the output
-    curl_output=$(curl -s -X GET -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" "$url")
+echo "$curl_output_first" >> tempfile_output_curl
 
-    # Check if the cURL output contains the error message
-    if [[ $curl_output == *"\"status\": \"NOT_FOUND\""* ]]; then
-        os_packages+=("$package")
+curl_output=$(curl -X GET -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) \
+  "https://artifactregistry.googleapis.com/v1/projects/cloud-aoss/locations/us/repositories/cloud-aoss-python/pythonPackages")
+
+next_page_token=$(echo "$curl_output" | grep nextPageToken | awk '{print $2}' | sed 's/"//g')
+
+while [[ -n "$next_page_token" ]]; do
+    curl_output_token=$(curl -sS -X GET -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+        "https://artifactregistry.googleapis.com/v1/projects/cloud-aoss/locations/us/repositories/cloud-aoss-python/pythonPackages?pageSize=2000&pageToken=$next_page_token")
+    echo "$curl_output_token" | grep name | sort -f >> tempfile_output_curl
+    next_page_token=$(echo "$curl_output_token" | grep nextPageToken | awk '{print $2}' | sed 's/"//g')
+done
+
+# Initialize counters and variables
+aoss_count=0
+os_count=0
+aoss_packages=""
+os_packages=""
+
+# Check which files from tempfile_output_pip are present in tempfile_output_curl
+while IFS= read -r file; do
+    if grep -q "$file" tempfile_output_curl; then
+        ((aoss_count++))
+        aoss_packages+="$(basename "$file" | awk -F'/' '{print $(NF-1)}')"$'\n'
+
     else
-        aoss_packages+=("$package")
+        ((os_count++))
     fi
-done < report.txt
+done < tempfile_output_pip
 
-# Create the report.txt file
-report_file="report.txt"
+# Save the final result in report.txt
+echo "Number of packages coming from AOSS: $aoss_count" > report.txt
+echo "Number of packages coming from public repository: $os_count" >> report.txt
+echo "List of packages coming from AOSS:" >> report.txt
+echo "----" >> report.txt
+echo "$aoss_packages" >> report.txt
 
-echo "Packages coming from Assured OSS: " > "$report_file"
-echo >> "$report_file"
-# Append the AOSS packages to the report file
-for package in "${aoss_packages[@]}"; do
-    echo "$package" >> "$report_file"
-done
-echo >> "$report_file"
-
-echo "Packages coming from Open Source: " >> "$report_file"
-echo >> "$report_file"
-# Append the OS packages to the report file
-for package in "${os_packages[@]}"; do
-    echo "$package" >> "$report_file"
-done
-
-# Print the report
-cat "$report_file"
-
-
-
+# Clean up by removing the temporary files
+rm tempfile_output_pip tempfile_output_curl
